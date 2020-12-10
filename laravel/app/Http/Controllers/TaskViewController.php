f7eac3088d0270fa2228d5cb6936d7f9701c7f19
tetreaum <tetreaum@mail.gvsu.edu>
2020-12-10 14:07:54 -0500

    retrieve data from database, clean up code

---
+++ laravel/app/Http/Controllers/TaskViewController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class TaskViewController extends Controller
+{
+    public function index()
+    {
+        $tasks = DB::select('SELECT t.TaskId, t.UpdateTime, t.TaskDesc, p.PrioType, t.NameAssigned, t.DueDate, s.StatusType
+        FROM Task t, Priority p, TaskStatus s
+        WHERE t.PrioId = p.PrioId AND t.StatusId = s.StatusId');
+        return view('welcome', compact('tasks'));
+    }
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskViewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [TaskViewController::class, 'index']);
+
+
